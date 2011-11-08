b7edf494070c902dfba35f8b0382ccee23d18d57
Alex Robbins <alexander.j.robbins@gmail.com>
2011-11-07 21:02:52 -0600

    First pass and it mostly works.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject three-source "1.0.0-SNAPSHOT"
             :description "FIXME: write description"
-            :dependencies [[org.clojure/clojure "1.2.1"]
+            :dependencies [[cheshire "2.0.2"]
+                           [clj-http "0.2.3"]
+                           [org.clojure/clojure "1.2.1"]
                            [ring/ring-core "1.0.0-RC1"]
                            [ring/ring-jetty-adapter "1.0.0-RC1"]
                            [ring-reload-modified "0.1.1"]
--- src/three_source/core.clj
+++ src/three_source/core.clj
@@ -1,23 +1,22 @@
 (ns three-source.core
   (:use compojure.core
-        [hiccup.page-helpers :only [html5 link-to url]]
+        [cheshire.core :only [parse-string]]
+        [hiccup.page-helpers :only [html5 link-to url include-css]]
         [hiccup.core :only [html defelem]]
         [hiccup.form-helpers :only [form-to text-field submit-button]]
         [ring.util.response :only [redirect]])
   (:require [compojure.route :as route]
-            [compojure.handler :as handler]))
+            [compojure.handler :as handler]
+            [clj-http.client :as client]))
 
 
 ; Template helpers
-(defn layout [title content]
+(defn base-template [title & content]
   (html5
-    [:head [:title title]]
-    [:body
-     (link-to
-       "/"
-       [:img {:src "/3sourceSmall.png"}])
-     [:h1 title]
-     content]))
+    [:head
+     [:title title]
+     (include-css "/screen.css")]
+    [:body content]))
 
 (defn who-did-what
   ([] (who-did-what "" "" "" ""))
@@ -33,25 +32,54 @@
                  [:td (text-field "pred" pred)]
                  [:td (text-field "e2" e2)]
                  [:td (text-field "prep" prep)]
-                 [:td (submit-button "Go")]]])]
-     [:div {:class "samples"}
-      (link-to (url "/results" {:e1 "zombies" :pred "eat"})
-               "What do zombies eat?")])))
+                 [:td (submit-button "Go")]]])])))
 
 
 ; View functions
 
 (defn home []
-  (layout "3Source"
-          (who-did-what)))
+  (base-template "3Source"
+                 [:div {:class "title"} [:img {:src "/3source.png"}]]
+                 (who-did-what)
+                 [:div {:class "samples"}
+                  (link-to (url "/results" {:e1 "zombies" :pred "eat"})
+                           "What do zombies eat?")]))
 
 (defn results [e1 pred e2 prep]
-  (if (every? empty? [e1 pred e2 prep])
-    (redirect "/")
-    (layout "Results of your query"
-            [:p 
-             (interpose ", "
-                        [e1 pred e2 prep])])))
+  (letfn
+    [(get-json [e1 pred e2 prep]
+               (:body
+                 (client/get
+                   (str
+                     "http://demo.languagecomputer.com/"
+                     (url "3source/json"
+                          {:e1 e1 :pred pred :e2 e2 :prep prep})))))
+     (parse-json [json-str]
+                 (parse-string json-str))
+     (result-table-row [{s1 "a0" s2 "a1" pred "pred"} cls]
+                       [:tr {:class cls}
+                        [:td s1]
+                        [:td pred]
+                        [:td s2]])]
+    (if (every? empty? [e1 pred e2 prep])
+      (redirect "/")
+      (base-template "Results"
+                     (link-to "/" [:img {:src "/3sourceSmall.png"}])
+                     (who-did-what e1 pred e2 prep)
+                     (let [data (parse-json
+                                  (get-json e1 pred e2 prep))
+                           result-count (count data)]
+                       [:div {:class "results"}
+                        [:p (str result-count
+                                 (if (= result-count 1)
+                                   " result"
+                                   " results"))]
+                        (if (> result-count 0)
+                          [:table
+                           (map result-table-row data
+                                (cycle ["odd" "even"]))]
+                          [:p
+                           "No results. Try searching with fewer terms."])])))))
 
 
 ; Routes
